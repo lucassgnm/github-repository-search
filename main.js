0873eb5e7f53ab73d0bd86c454935d0acb8d1e0b
Lucas Giovani <lucasgnm@gmail.com>
2020-04-28 11:56:07 -0300

    user facilities added

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ var inputElement = document.querySelector('#app input');
 var ulElement = document.querySelector('#app ul');
 var buttonElement = document.querySelector('#app button');
 
+inputElement.addEventListener('keyup', function (e) {
+    var key = e.which || e.keyCode;
+    if (key == 13) {
+        sendUser();
+    }
+})
+
 function sendUser() {
     ulElement.innerHTML = '';
     var user = inputElement.value;
@@ -10,20 +17,20 @@ function sendUser() {
 
 function getGitRepository(inputUser) {
     var url = 'https://api.github.com/users/';
-    var urlUser = url+inputUser+'/repos';
+    var urlUser = url + inputUser + '/repos';
 
     axios.get(urlUser)
-    .then(function(response) {
-        for (rep of response.data) {
-            var elementData = document.createElement('li');
-            var elementText = document.createTextNode(rep.name);
+        .then(function (response) {
+            for (rep of response.data) {
+                var elementData = document.createElement('li');
+                var elementText = document.createTextNode(rep.name);
 
-            elementData.appendChild(elementText);
-            ulElement.appendChild(elementData);
-        }
-    })
-    .catch(function(error) {
-        console.warn(error);
-    });
+                elementData.appendChild(elementText);
+                ulElement.appendChild(elementData);
+            }
+        })
+        .catch(function (error) {
+            console.warn(error);
+        });
     inputElement.value = '';
 }
